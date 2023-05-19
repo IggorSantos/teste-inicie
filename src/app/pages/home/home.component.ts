413ccc436c83d7c2071527a6663a7ee234ec76c6
Igor Santos <iggor.jsantos@gmail.com>
2023-05-18 22:51:33 -0300

    fix(home): pegando informações dos cards

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -8,23 +8,38 @@ import { PokemonListService } from 'src/app/services/pokemon-list.service';
 })
 export class HomeComponent implements OnInit {
   pokemons: any;
+  pokes: Array<any> = [];
 
   constructor(private service: PokemonListService) { }
 
   ngOnInit(): void {
     this.getPokemons()
+    //this.getPokemon()
   }
 
   getPokemons(){
     this.service.getPokemons().subscribe({
       next: (res: any)=> {  
         this.pokemons = res.results      
-        console.log(this.pokemons)
+        this.pokemons.forEach((pokemon: any) => {
+          this.getPokemon(pokemon)          
+        });
       },
       error: (error: any) => {
         console.error(error)
       }
     })
+  }
+
+  getPokemon(pokemon: any){
+    this.service.getPokemon(pokemon.url).subscribe({
+      next: (res: any)=> {               
+        this.pokes.push(res)
+      },
+      error: (error: any) => {
+        console.error(error)
+      }
+    }) 
   }
 
 }
